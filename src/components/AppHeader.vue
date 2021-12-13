3231aa5a33a1209819ec1aec4087ae173353da66
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-13 12:46:54 +0100

    Update macro structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,43 @@
 <template>
-    <div>
+    <div class="clearfix">
+        <!-- text column  -->
+        <div class="text-col">
+            <!-- logo -->
+            <img src="" alt="">
 
+            <!-- content  -->
+            <div class="content">
+                <div class="content-top">
+                    <!-- line  -->
+                    <div></div>
+
+                    <h3></h3>
+                </div>
+
+                <div class="content-bottom">
+                    <!-- title  -->
+                    <h1></h1>
+
+                    <!-- text  -->
+                    <p></p>
+
+                    <!-- button  -->
+                    <div></div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <!-- image column  -->
+        <div class="image-col">
+            <!-- nav menu  -->
+            <nav>
+                <ul>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+
+        </div>
     </div>
 </template>
 
@@ -12,5 +49,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 
 </style>
