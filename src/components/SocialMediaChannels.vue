4cfc9e25087830f307db4aa08ffb68bf12362aa3
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-14 12:07:18 +0100

    Create new component SocialMediaChannels

---
+++ src/components/SocialMediaChannels.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul id="sm-channels">
+        <!-- con un ciclo vado a stampare tutti gli oggetti contenuti nell'array smChannels presente nel componente padre AppFooter -->
+        <li v-for="channel, i in channels" :key="i">
+            <a :href="channel.url">
+                <i :class="channel.icon"></i>
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'SocialMediaChannels',
+  props: {
+    //   ricevo le info dei canali social dal componente padre AppFooter 
+      channels: Array,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
