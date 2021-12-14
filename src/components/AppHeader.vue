8662e107fccaf583cd04000f1a5ffc6f3dd45349
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-14 19:06:44 +0100

    Refactoring component AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,67 +1,49 @@
 <template>
     <section>
-        <!-- header navigation bar  -->
-        <div id="header-nav" class="clearfix">
-            <!-- logo  -->
-            <div id="logo">
-                <img src="../assets/img/logo-restaurant.png" alt=" Avada restaurant logo">
+        <div class="text-col">
+            <!-- logo -->
+            <img src="../assets/img/logo-restaurant.png" alt=" Avada restaurant logo">
+            <!-- content  -->
+            <div class="content">
+                <div class="content-top">
+                    <!-- line  -->
+                    <div class="line"></div>
+                    <h3>The best table in town </h3>
+                </div>
+                <div class="content-bottom">
+                    <!-- title  -->
+                    <h1>Fine dining experience</h1>
+                    <!-- text  -->
+                    <p>Pellentesque vitae viverra risus, sagitis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.</p>
+                    <!-- button  -->
+                    <div class="btn bg-white">Explore the menu</div>
+                    
+                </div>
             </div>
+        </div>
 
-            <!-- nav menu  -->
-            <div class="nav-container">
+        <!-- image column  -->
+        <div class="image-col">
+            <div class="overlay">
+                <!-- nav menu  -->
+                <div class="nav-container">
                     <nav>
                         <ul>
                             <li v-for="link, i in links" :key="i">
                                 <a v-if="link.icon" :href="link.url">
-                                    <i :class="link.icon"></i> 
+                                    <i :class="link.icon"></i>
                                 </a>
+
                                 <a v-else :href="link.url" :class="link.here ? 'active' : ''">
-                                    {{ link.text }} 
-                                    
+                                    {{ link.text }}
                                     <span class="new" v-if="link.badge">
                                         {{ link.badge }}
-                                    </span>
+                                    </span> 
                                 </a>
                             </li>
                         </ul>
                     </nav>
                 </div>
-        </div>
-
-
-        <!-- jumbo  -->
-        <div id="jumbo-container" class="clearfix">
-            <!-- text column  -->
-            <div class="text-col">
-                <div class="content">
-                    <div class="content-top">
-                        <!-- line  -->
-                        <div class="line"></div>
-
-                        <h3>The best table in town </h3>
-                    </div>
-
-                    <div class="content-bottom">
-                        <!-- title  -->
-                        <h1>Fine dining experience</h1>
-
-                        <!-- text  -->
-                        <div class="text-container">
-                            <p>
-                                Pellentesque vitae viverra risus, sagitis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.
-                            </p>
-                        </div>
-
-                        <!-- button  -->
-                        <div class="btn bg-white">Explore the menu</div>
-                        
-                    </div>
-                </div>
-            </div>
-
-            <!-- image column  -->
-            <div class="image-col">
-                <div class="overlay"></div>
             </div>
         </div>
     </section>
@@ -81,161 +63,123 @@ export default {
 <style scoped lang="scss">
 section {
     height: 715px;
-    position: relative;
-
-    #header-nav {
-        width: 95%;
-        margin: 0 auto;
-        padding: 20px 0;
-        line-height: 80px;
-        z-index: 1;
-
-        #logo {
-            float: left;
-
-            img {
-                line-height: 80px;
-                vertical-align: middle;
-                margin-left: 45px;
+    .text-col {
+        width: 45%;
+        height: 100%;
+        float: left;
+        color: white;
+        background-color: rgba(3,3,3,255);
+        padding: 40px 80px 0 80px;
+    
+        .content {
+            margin: 120px 0;
+    
+            .content-top {
+                display: flex;
+                align-items: center;
+    
+                .line {
+                    width: 60px;
+                    border-bottom: 2px solid white;
+                    margin-right: 60px;
+                }
+    
+                h3 {
+                    font-size: 15px;
+                    font-weight: normal;
+                    text-transform: uppercase;
+                }
+            }
+    
+            .content-bottom {
+                // margin: 0 50px 0 120px;
+                margin-left: 120px;
+    
+                h1 {
+                    font-size: 55px;
+                    text-transform: uppercase;
+                    margin: 40px 0;
+                }
+    
+                p {
+                    line-height: 30px;
+                    margin-bottom: 40px;
+                }
+    
+                .btn {
+                    text-transform: uppercase;
+                    font-size: 12px;
+                    border: 1px solid white;
+                    padding: 10px 30px;
+                    display: inline;
+                    cursor: pointer;
+    
+                    &.bg-white:hover{
+                        color: black;
+                        background-color: white;
+                    }
+                }
             }
         }
+    }
 
-        .nav-container {
-            float: right;
+    .image-col {
+        width: 55%;
+        height: 100%;
+        float: left;
+        position: relative;
+        background-image: url("../assets/img/slider52x.jpg");
+        background-size: cover;
 
-            nav {
-                margin-right: 15px;
+        .overlay {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background-color: rgba(0, 0, 0, 0.2);
 
+            .nav-container {
+                position: absolute;
+                top: 55px;
+                width: 100%;
+    
                 ul {
+                    display: flex;
+                    justify-content: center;
+                    list-style: none;
 
                     li {
-                        display: inline-block;
-                        margin-right: 18px;
-
+                        margin-right: 28px;
+    
                         a {
-                            font-size: 15px;
+                            font-size: 12px;
                             color: #9c958f;
                             text-decoration: none;
                             font-weight: bold;
 
+                            &.active {
+                                color: white;
+                            }
+
                             .new {
-                                line-height: 80px;
-                                vertical-align: middle;
                                 font-size: 10px;
                                 padding: 2px 8px;
                                 border: 1px solid white;
                                 margin-left: 5px;
                             }
-
-                            &.active {
-                                color: white;
-                            }
-                        }
-                    }
-                }
-            }
-        
-        }
-    }
-
-    #jumbo-container {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1;
-
-        .text-col {
-            width: 45%;
-            height: 100%;
-            float: left;
-            color: white;
-            background-color: rgba(3,3,3,255);
-            padding: 40px 80px 0 80px;
-        
-            .content {
-                margin: 120px 0;
-        
-                .content-top {
-                    display: flex;
-                    align-items: center;
-        
-                    .line {
-                        width: 60px;
-                        border-bottom: 2px solid white;
-                        margin-right: 60px;
-                    }
-        
-                    h3 {
-                        font-size: 15px;
-                        font-weight: normal;
-                        text-transform: uppercase;
-                    }
-                }
-        
-                .content-bottom {
-                    margin-left: 120px;
-        
-                    h1 {
-                        font-size: 55px;
-                        text-transform: uppercase;
-                        line-height: 75px;
-                        margin: 40px 0;
-                    }
-
-                    .text-container {
-                        max-height: 150px;
-                        overflow-y: auto;
-                        margin-bottom: 20px;
-
-                        p {
-                            line-height: 30px;
-                            margin-bottom: 40px;
-                        }
-                    }
-        
-                    .btn {
-                        text-transform: uppercase;
-                        font-size: 12px;
-                        border: 1px solid white;
-                        padding: 10px 30px;
-                        display: inline;
-                        cursor: pointer;
-        
-                        &.bg-white:hover{
-                            color: black;
-                            background-color: white;
                         }
                     }
                 }
             }
         }
-
-        .image-col {
-            width: 55%;
-            height: 100%;
-            float: left;
-            background-image: url("../assets/img/slider52x.jpg");
-            background-size: cover;
-
-            .overlay {
-                position: absolute;
-                top: 0;
-                right: 0;
-                bottom: 0;
-                left: 0;
-                background-color: rgba(0, 0, 0, 0.2);
-            }
-        }
     }
-
-    .clearfix::after {
-        content: "";
-        display: table;
-        clear: both;
-    }
-
 }
+.section::after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
 
 </style>
