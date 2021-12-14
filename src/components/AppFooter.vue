d8503b62b407ec616b331910ebb6ef71e9670fa6
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-14 10:46:18 +0100

    Create macro structure in AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
+    <div id="footer">
+      <!-- text  -->
+      <div id="footer-text">
+        <div class="content">
+          <!-- logo  -->
+          <img src="" alt="">
+
+          <!-- text  -->
+          <p></p>
+
+          <!-- social media channels -->
+          <div id="sm-channels">
+
+          </div>
+
+        </div>
+      </div>
+
+      <!-- copyright  -->
+      <div id="copyright">
+        <span></span>
+      </div>
 
     </div>
 </template>
