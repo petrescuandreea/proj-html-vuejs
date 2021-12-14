5e09a5f74911051407cdf309549bee811cb38a8b
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-14 14:25:06 +0100

    Add content to component AppSection1

---
+++ src/components/AppSection1.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+      <div class="center container">
+        <!-- text column  -->
+        <div class="text-col">
+            <div class="content">
+                <p>
+                    <strong style="text-transform:uppercase">hac tellus, felis risus at</strong>
+                    mattis mattis. Eget euismod semper eget tortor, donee amet, blandit. Tristique facilisi faucibus elementum feugiat in nam in feugiat. Ipsum odio etiam duis facilisis amet vulputate.
+                </p>
+
+                <h2>
+                    food is our common ground, a universal experience.
+                </h2>
+
+                <p>
+                    Tristique tempus condimentum diam donee. Condimentum ullamcorper sit elementum hendrerit mi nulla in consequat, ut. Metus, nullam seelerisque netus viverra dui.
+                </p>
+
+                <img src="../assets/img/signature.jpg" alt="signature">
+            </div>
+        </div>
+
+        <!-- image column  -->
+        <div class="image-col">
+            <img src="../assets/img/info22x.jpg" alt="California Rolls image">
+        </div>
+
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppSection1',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
