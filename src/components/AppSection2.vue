3aedf71de062a18389d2ad2c2c773cd2222ea1ee
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-14 15:58:13 +0100

    Create macro structure in component AppSection2

---
+++ src/components/AppSection2.vue
@@ -0,0 +1,47 @@
+<template>
+  <section id="reservations">
+      <div class="center-container">
+          <div class="card">
+              <img src="" alt="">
+
+              <h3></h3>
+
+              <p></p>
+
+              <div class="btn"></div>
+          </div>
+
+          <div class="card">
+              <img src="" alt="">
+
+              <h3></h3>
+
+              <p></p>
+              
+              <div class="btn"></div>
+          </div>
+
+          <div class="card">
+              <img src="" alt="">
+
+              <h3></h3>
+
+              <p></p>
+              
+              <div class="btn"></div>
+          </div>
+      </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppSection2',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
