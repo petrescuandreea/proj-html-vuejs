85b1d8e2946d33c778b8bbfd10e3456e483e5170
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-14 20:00:43 +0100

    Create macro structure in component Team

---
+++ src/components/Team.vue
@@ -0,0 +1,90 @@
+<template>
+    <section id="team">
+        <div class="center-container">
+            <h2>
+
+            </h2>
+
+            <div class="extend-card">
+                <!-- image column  -->
+                <div class="image-col">
+                    <img src="" alt="">
+                </div>
+
+                <!-- text column  -->
+                <div class="text-col">
+                    <!-- member name  -->
+                    <span></span>
+
+                    <!-- daily newspaper name  -->
+                    <span></span>
+
+                    <!-- stars  -->
+                    <div></div>
+
+                    <!-- line  -->
+                    <div></div>
+
+                    <!-- text  -->
+                    <p></p>
+
+                    <!-- button  -->
+                    <button></button>
+                </div>
+            </div>
+
+            <div class="card">
+                <!-- member name  -->
+                <span></span>
+
+                <!-- daily newspaper name  -->
+                <span></span>
+
+                <!-- stars  -->
+                <div></div>
+
+                <!-- line  -->
+                <div></div>
+
+                <!-- text  -->
+                <p></p>
+
+                <!-- button  -->
+                <button></button>
+            </div>
+
+            <div class="card">
+                 <!-- member name  -->
+                <span></span>
+
+                <!-- daily newspaper name  -->
+                <span></span>
+
+                <!-- stars  -->
+                <div></div>
+
+                <!-- line  -->
+                <div></div>
+
+                <!-- text  -->
+                <p></p>
+
+                <!-- button  -->
+                <button></button>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Team',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
