04709d53d9186eb343636dbf24bdcdadc15e3d23
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-15 12:10:10 +0100

    Create macro structure of component Bulletin

---
+++ src/components/Bulletin.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="bulletin">
+      <div class="center-container">
+          <h2></h2>
+
+          <div class="line"></div>
+
+          <!-- da stampare dinamicamente  -->
+          <div class="card">
+              <div class="card-img">
+                  <img src="" alt="">
+              </div>
+
+              <div class="card-text">
+                  <h3></h3>
+                  <p></p>
+              </div>
+          </div>
+      </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Bulletin',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
