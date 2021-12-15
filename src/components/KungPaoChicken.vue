29d871e0e0aeea4fbd747705f132453193c1b7aa
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-15 11:39:04 +0100

    Add content to component KungPaoChicken

---
+++ src/components/KungPaoChicken.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id="kung-pao-chicken">
+      <span>kung pao chicken - &#65129;22</span>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'KungPaoChicken',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
