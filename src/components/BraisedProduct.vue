7660af3eb0fbe39b23d8b68efd9826c58cbf2a3b
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-15 15:51:50 +0100

    Add content in component BraisedProduct

---
+++ src/components/BraisedProduct.vue
@@ -0,0 +1,39 @@
+<template>
+    <section id="braised-abalone-product">
+        <div class="content-container">
+            <!-- title  -->
+            <div class="title">
+                <h2></h2>
+                <span class="price">
+                    &#36;52
+                </span>
+            </div>
+
+            <!-- line  -->
+            <div class="line"></div>
+
+            <!-- text  -->
+            <div class="text">
+                <p>
+                    Pretium accumsan porttitor viverra leo gravida mollis imperdiet. Fringilla nibh pharetra sociis leo amet.
+                </p>
+            </div>
+
+            <!-- button  -->
+            <button>
+                View in the menu
+            </button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'BraisedProduct',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
