25043596e16c7801c66a22c7ca52b6b125720544
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-15 16:47:07 +0100

    Add style to component BraisedProduct

--- src/components/BraisedProduct.vue
+++ src/components/BraisedProduct.vue
@@ -36,10 +36,61 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #braised-abalone-product {
-    height: 700px;
     background-image: url("../assets/img/hero42x.jpg");
     background-size: cover;
     background-position: center;
+    padding: 80px 0 80px 200px;
+
+    .content-container {
+        background-color: black;
+        color: #676059;
+        width: 30%;
+        padding: 80px 60px;
+
+        .title {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+            color: white;
+
+            h2 {
+                font-size: 38px;
+                text-transform: uppercase;
+                line-height: 50px;
+                width: 50%;
+                margin-bottom: 60px;
+            }
+        }
+
+        .line {
+            width: 60px;
+            border-bottom: 2px solid;
+            margin-right: 60px;
+            margin-bottom: 40px;
+        }
+
+        .text {
+            max-height: 150px;
+            overflow: hidden;
+        }
+
+        button {
+            text-transform: uppercase;
+            font-size: 12px;
+            border: 1px solid white;
+            background-color: black;
+            color: white;
+            padding: 10px 30px;
+            margin-top: 40px;
+            cursor: pointer;
+
+            &:hover{
+                color: black;
+                background-color: white;
+            }
+        }
+
+    }
 }
 
 </style>
